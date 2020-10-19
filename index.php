3e88c8dfb35e9c459b5d5a3c812c90d213c6b6d9
inspiration2030 <40746088+inspiration2030@users.noreply.github.com>
2020-10-19 14:46:51 +0800

    我是傻子
    我单知道mdui会针对移动端优化，却不知还要设置meta标签。
    我是傻子

--- index.php
+++ index.php
@@ -1,16 +1,20 @@
 <!DOCTYPE html>
 <html>
     <head>
+        <meta
+              name = "viewport"
+              content = "width = device-width, initial-scale = 1.0, maximum-scale = 1.0, user-scalable = no"
+        >
         <link
-            rel="stylesheet"
-            href="https://cdn.jsdelivr.net/npm/mdui@1.0.0/dist/css/mdui.min.css"
-            integrity="sha384-2PJ2u4NYg6jCNNpv3i1hK9AoAqODy6CdiC+gYiL2DVx+ku5wzJMFNdE3RoWfBIRP"
-            crossorigin="anonymous"
+            rel = "stylesheet"
+            href = "https://cdn.jsdelivr.net/npm/mdui@1.0.0/dist/css/mdui.min.css"
+            integrity = "sha384-2PJ2u4NYg6jCNNpv3i1hK9AoAqODy6CdiC+gYiL2DVx+ku5wzJMFNdE3RoWfBIRP"
+            crossorigin = "anonymous"
         />
         <script
-            src="https://cdn.jsdelivr.net/npm/mdui@1.0.0/dist/js/mdui.min.js"
-            integrity="sha384-aB8rnkAu/GBsQ1q6dwTySnlrrbhqDwrDnpVHR2Wgm8pWLbwUnzDcIROX3VvCbaK+"
-            crossorigin="anonymous"
+            src = "https://cdn.jsdelivr.net/npm/mdui@1.0.0/dist/js/mdui.min.js"
+            integrity = "sha384-aB8rnkAu/GBsQ1q6dwTySnlrrbhqDwrDnpVHR2Wgm8pWLbwUnzDcIROX3VvCbaK+"
+            crossorigin = "anonymous"
         ></script>
         <style>
             .empty{
